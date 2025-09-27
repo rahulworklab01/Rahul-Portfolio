d1c6bb503c0a417fdde1c242aa5eec702f3a23ed
Harshh004 <harshgupta21q@gmail.com>
2025-09-27 14:40:46 +0530

    updated links

--- assets/js/script.js
+++ assets/js/script.js
@@ -142,7 +142,10 @@ const pages = document.querySelectorAll("[data-page]");
 
 // add event to all nav link
 for (let i = 0; i < navigationLinks.length; i++) {
-  navigationLinks[i].addEventListener("click", function () {
+  navigationLinks[i].addEventListener("click", function (event) {
+    if (event.target.tagName === 'A' && event.target.target === '_blank') {
+      return;
+    }
 
     for (let i = 0; i < pages.length; i++) {
       if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
